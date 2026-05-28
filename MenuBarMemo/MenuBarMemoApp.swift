3add1a8e0f88842b5beea3db48ca088d93dbecac
dingdingcar <1054263492@qq.com>
2026-05-28 16:53:54 +0800

    Initial Commit

---
+++ MenuBarMemo/MenuBarMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarMemoApp.swift
+//  MenuBarMemo
+//
+//  Created by 丁泓景 on 2026/5/28.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
